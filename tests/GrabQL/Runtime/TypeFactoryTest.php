ff9d5088ba5cffb2e864496a3431ee0f5d998407
Alberto Arena <arena.alberto@gmail.com>
2015-02-19 23:17:58 +0000

    Complete unit test for TypeFactory

--- tests/GrabQL/Runtime/TypeFactoryTest.php
+++ tests/GrabQL/Runtime/TypeFactoryTest.php
@@ -2,6 +2,23 @@
 use \GrabQL\Runtime\TypeFactory;
 use \GrabQL\Runtime\Type\Scalar;
 
+class MockTypeFactory
+{
+    public $name;
+
+    public $surname;
+
+    public function __construct($name, $surname) {
+        $this->name = $name;
+        $this->surname = $surname;
+    }
+
+    public function getNameSurname()
+    {
+        return $this->name . ' ' . $this->surname;
+    }
+}
+
 class TypeFactoryTest extends \PHPUnit_Framework_TestCase
 {
 
@@ -43,4 +60,62 @@ class TypeFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('hello', $instance->execute());
     }
 
+    /**
+     * @covers \GrabQL\Runtime\TypeFactory::createInstance
+     * @covers \GrabQL\Runtime\Type\Scalar::init
+     * @covers \GrabQL\Runtime\Type\Map::init
+     * @covers \GrabQL\Runtime\Type\Map::initFromArray
+     * @covers \GrabQL\Runtime\Type\Map::atImplicit
+     * @covers \GrabQL\Runtime\Type\Map::count
+     */
+    public function testArray()
+    {
+        $instance = TypeFactory::createInstance('id', array(1, 2, 3));
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Map', $instance);
+        $this->assertEquals('id', $instance->getId());
+        $this->assertEquals(3, $instance->count());
+    }
+
+    /**
+     * @covers \GrabQL\Runtime\TypeFactory::createInstance
+     * @covers \GrabQL\Runtime\Type\Scalar::init
+     * @covers \GrabQL\Runtime\Type\Map::init
+     * @covers \GrabQL\Runtime\Type\Map::initFromArray
+     * @covers \GrabQL\Runtime\Type\Map::atImplicit
+     * @covers \GrabQL\Runtime\Type\Map::count
+     */
+    public function testObject()
+    {
+        $instance = TypeFactory::createInstance('id', new MockTypeFactory('John', 'Smith'));
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Map', $instance);
+        $this->assertEquals('id', $instance->getId());
+        $this->assertEquals(3, $instance->count());
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Scalar', $instance->at('name'));
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Scalar', $instance->at('surname'));
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Procedure', $instance->at('getNameSurname'));
+    }
+
+    /**
+     * @covers \GrabQL\Runtime\TypeFactory::createInstance
+     * @covers \GrabQL\Runtime\Type\Scalar::init
+     * @covers \GrabQL\Runtime\Type\Resource::init
+     */
+    public function testResource()
+    {
+        $instance = TypeFactory::createInstance('id', @fopen('./composer.json', 'r'));
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Resource', $instance);
+        $this->assertEquals('id', $instance->getId());
+    }
+
+    /**
+     * @covers \GrabQL\Runtime\TypeFactory::createInstance
+     * @covers \GrabQL\Runtime\Type\Scalar::init
+     * @covers \GrabQL\Runtime\Type\Nil::init
+     */
+    public function testNil()
+    {
+        $instance = TypeFactory::createInstance('id', null);
+        $this->assertInstanceOf('\\GrabQL\\Runtime\\Type\\Nil', $instance);
+        $this->assertEquals('id', $instance->getId());
+    }
 } 
